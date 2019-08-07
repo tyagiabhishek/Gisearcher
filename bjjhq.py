d97291f7da416ecc5b2762146f10c848b404d08f
tyagiabhishek <tyagiabhishek13@gmail.com>
2019-08-07 14:09:06 +0530

    without password email

--- bjjhq.py
+++ bjjhq.py
@@ -3,7 +3,7 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 import re
-import yagmail
+import smtplib,ssl
 
 def linkmmahq(soup):
     headings = soup.findAll('h1')
@@ -30,14 +30,15 @@ def get_link_name(url):
         return linkbjjfanatics(soup)
     elif "mmahq" in url:
         return linkmmahq(soup)
-    
+
+port = 465   
+smtp_server = "smtp.gmail.com"
 sender_email = "leo7279bot@gmail.com"
 receiver_email = "tyagiabhishek13@gmail.com"
-message = "The following Gi is on sale: "
-yag = yagmail.SMTP("leo7279bot@gmail.com")
+password = "Kakashiken@123"
 Gionsale = set()
-urls = [ 'https://www.bjjhq.com/','https://bjjfanatics.com/' ]
-
+urls = [ 'https://www.bjjhq.com/','https://bjjfanatics.com/collections/daily-deals','https://www.mmahq.com/' ]
+context = ssl.create_default_context()
 #working loop
 while True:
     for url in urls:
@@ -45,10 +46,13 @@ while True:
         if re.search('gi',saleitem,re.IGNORECASE):
             if not saleitem in Gionsale:
                 Gionsale.add(saleitem)
+                message = "Subject: BOT NOTIFICATION Gi On Sale\n"
                 message = "The following Gi is on sale: "
                 message+= saleitem +"\n"
                 message += "Get in on: "+link
-                yag.send(to = receiver_email,subject = "BOT NOTIFICATION: Gi on sale",contents=message)
+                with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                    server.login(sender_email, password)
+                    server.sendmail(sender_email, receiver_email, message)
                 
                 print("New Gi on sale "+saleitem)
     time.sleep(5*60)
